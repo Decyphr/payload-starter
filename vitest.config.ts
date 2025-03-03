11fc1611d9c6a890bc22b9353a46b328616dbabc
Decyphr <blakeha94@gmail.com>
2025-03-03 09:25:57 -0500

    remove: storybook testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
 
-import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import "dotenv/config";
@@ -31,30 +30,30 @@ export default defineConfig({
           environment: "jsdom",
         },
       },
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, ".storybook"),
-          }),
-        ],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: "playwright",
-            instances: [
-              {
-                browser: "chromium",
-              },
-            ],
-          },
-          setupFiles: [".storybook/vitest.setup.ts"],
-        },
-      },
+      // {
+      //   extends: true,
+      //   plugins: [
+      //     // The plugin will run tests for the stories defined in your Storybook config
+      //     // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+      //     storybookTest({
+      //       configDir: path.join(dirname, ".storybook"),
+      //     }),
+      //   ],
+      //   test: {
+      //     name: "storybook",
+      //     browser: {
+      //       enabled: true,
+      //       headless: true,
+      //       provider: "playwright",
+      //       instances: [
+      //         {
+      //           browser: "chromium",
+      //         },
+      //       ],
+      //     },
+      //     setupFiles: [".storybook/vitest.setup.ts"],
+      //   },
+      // },
     ],
     include: ["src/**/*.test.tsx", "src/**/*.test.ts"],
     coverage: {
