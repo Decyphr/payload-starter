16145a16196d41abd4680030ab94ddba046927ad
Decyphr <blakeha94@gmail.com>
2025-03-04 17:10:56 -0500

    add: media prefix migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250129_190005 from './20250129_190005';
+import * as migration_20250304_220800 from './20250304_220800';
 
 export const migrations = [
   {
     up: migration_20250129_190005.up,
     down: migration_20250129_190005.down,
-    name: '20250129_190005'
+    name: '20250129_190005',
+  },
+  {
+    up: migration_20250304_220800.up,
+    down: migration_20250304_220800.down,
+    name: '20250304_220800'
   },
 ];
