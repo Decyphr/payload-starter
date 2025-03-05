2b8b9fe4ab41e6670d1730aa0b5d081799a09bca
Decyphr <blakeha94@gmail.com>
2025-03-05 13:41:30 -0500

    add: pages to search index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250129_190005 from './20250129_190005';
 import * as migration_20250304_220800 from './20250304_220800';
+import * as migration_20250305_183904 from './20250305_183904';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250304_220800.up,
     down: migration_20250304_220800.down,
-    name: '20250304_220800'
+    name: '20250304_220800',
+  },
+  {
+    up: migration_20250305_183904.up,
+    down: migration_20250305_183904.down,
+    name: '20250305_183904'
   },
 ];
